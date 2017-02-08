7c2ac8ab7a17402e75cc2f3b57dbd4cd2b2be9ff
payal agrawal <pagrawal312@gmail.com>
2017-02-08 12:08:46 -0800

    added username and password

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       (process.env.NODE_ENV === 'production' ?
-                           'mongodb://<dbuser>:<dbpassword>@ds137139.mlab.com:37139/spaces' :
+                           'mongodb://space_user:space_user@ds137139.mlab.com:37139/spaces' :
                            'mongodb://localhost/spaces');
 exports.PORT = process.env.PORT || 8080;
 
